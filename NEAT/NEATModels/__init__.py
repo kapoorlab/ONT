9bbf0363e895624bd8cc397c1c0381c1cdd24e05
kapoorlab <randomaccessiblekapoor@gmail.com>
2020-05-23 15:16:59 +0200

    static net code

--- NEAT/NEATModels/__init__.py
+++ NEAT/NEATModels/__init__.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import, print_function
 
 from NEATModels.neat_standard import NEATDetection
 from NEATModels.neat_standard import yolo_loss
+from NEATModels.neat_static_standard import NEATStaticDetection
 from NEATModels.nets import Concat
 
 
