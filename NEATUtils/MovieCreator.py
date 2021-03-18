2f42a17f0843b59401c54a0721673b086a813a5f
kapoorlab <randomaccessiblekapoor@gmail.com>
2021-03-18 15:23:59 +0100

    ont code static

--- NEATUtils/MovieCreator.py
+++ NEATUtils/MovieCreator.py
@@ -1,22 +1,14 @@
 import csv
 import numpy as np
-from tifffile import imwrite 
+from tifffile import imread, imwrite 
 import pandas as pd
 import os
+import glob
 from skimage.measure import regionprops
 from skimage import measure
 from scipy import spatial 
-try:
-    from pathlib import Path
-    Path().expanduser()
-except (ImportError,AttributeError):
-    from pathlib2 import Path
+from pathlib import Path
 
-try:
-    import tempfile
-    tempfile.TemporaryDirectory
-except (ImportError,AttributeError):
-    from backports import tempfile
     
 """
 @author: Varun Kapoor
@@ -170,24 +162,50 @@ def MovieMaker(time, y, x, angle, image, segimage, crop_size, gridX, gridY, offs
        
    
                   
-    
-def CreateTrainingImages(csv_file, image, segimage, crop_size, TotalCategories, trainlabel, save_dir, gridX = 1, gridY = 1, offset = 0, defname = ""):
-
+def ImageLabelDataSet(ImageDir, SegImageDir, CSVDir,SaveDir, StaticName, StaticLabel, CSVNameDiff,crop_size, gridX = 1, gridY = 1, offset = 0):
     
     
-            #Check if the csv file exists
-            if os.path.exists(csv_file):
-                    Path(save_dir).mkdir(exist_ok=True)
-                    name = 1
-                    dataset = pd.read_csv(csv_file)
-                    time = dataset[dataset.keys()[0]][1:]
-                    y = dataset[dataset.keys()[1]][1:]
-                    x = dataset[dataset.keys()[2]][1:]                        
-                    
-                    #Categories + XYHW + Confidence 
-                    for t in x:
-                       ImageMaker(time[t], y[t], x[t], image, segimage, crop_size, gridX, gridY, offset, TotalCategories, trainlabel, str(name), save_dir)    
-                       name = name + 1
+            Raw_path = os.path.join(ImageDir, '*tif')
+            Seg_path = os.path.join(SegImageDir, '*tif')
+            Csv_path = os.path.join(CSVDir, '*csv')
+            filesRaw = glob.glob(Raw_path)
+            filesRaw.sort
+            filesSeg = glob.glob(Seg_path)
+            filesSeg.sort
+            filesCsv = glob.glob(Csv_path)
+            filesCsv.sort
+            Path(SaveDir).mkdir(exist_ok=True)
+            TotalCategories = len(StaticName)
+            count = 0
+            for csvfname in filesCsv:
+              print(csvfname)
+              CsvName =  os.path.basename(os.path.splitext(csvfname)[0])
+            
+              for fname in filesRaw:
+                  
+                 Name = os.path.basename(os.path.splitext(fname)[0])   
+                 for Segfname in filesSeg:
+                      
+                      SegName = os.path.basename(os.path.splitext(Segfname)[0])
+                        
+                      if Name == SegName:
+                          
+                          
+                         image = imread(fname)
+                         segimage = imread(Segfname)
+                         for i in  range(0, len(StaticName)):
+                             Eventname = StaticName[i]
+                             trainlabel = StaticLabel[i]
+                             if CsvName == CSVNameDiff + Name + Eventname:
+                                            dataset = pd.read_csv(csvfname)
+                                            time = dataset[dataset.keys()[0]][1:]
+                                            y = dataset[dataset.keys()[1]][1:]
+                                            x = dataset[dataset.keys()[2]][1:]                        
+                                            #Categories + XYHW + Confidence 
+                                            for t in range(1, len(time)):
+                                               ImageMaker(time[t], y[t], x[t], image, segimage, crop_size, gridX, gridY, offset, TotalCategories, trainlabel, Name + str(count), SaveDir)    
+                                               count = count + 1
+    
 
 
 
@@ -215,62 +233,62 @@ def  ImageMaker(time, y, x, image, segimage, crop_size, gridX, gridY, offset, To
           AllShifts = [shiftNone]
 
        
-       
-       currentsegimage = segimage[time,:].astype('uint16')
-       for shift in AllShifts:
-           
-                defaultX = int(x + shift[0])  
-                defaultY = int(y + shift[1])
-                #Get the closest centroid to the clicked point
-                properties = measure.regionprops(currentsegimage, currentsegimage)
-                TwoDCoordinates = [(prop.centroid[0], prop.centroid[1]) for prop in properties]
-                TwoDtree = spatial.cKDTree(TwoDCoordinates)
-                TwoDLocation = (defaultY,defaultX)
-                closestpoint = TwoDtree.query(TwoDLocation)
-                for prop in properties:
+       try:
+               currentsegimage = segimage[time,:].astype('uint16')
+               for shift in AllShifts:
+                   
+                        defaultX = int(x + shift[0])  
+                        defaultY = int(y + shift[1])
+                        #Get the closest centroid to the clicked point
+                        properties = measure.regionprops(currentsegimage, currentsegimage)
+                        TwoDCoordinates = [(prop.centroid[0], prop.centroid[1]) for prop in properties]
+                        TwoDtree = spatial.cKDTree(TwoDCoordinates)
+                        TwoDLocation = (defaultY,defaultX)
+                        closestpoint = TwoDtree.query(TwoDLocation)
+                        for prop in properties:
+                                           
+                                           
+                                    if int(prop.centroid[0]) == int(TwoDCoordinates[closestpoint[1]][0]) and int(prop.centroid[1]) == int(TwoDCoordinates[closestpoint[1]][1]):
+                                                        minr, minc, maxr, maxc = prop.bbox
+                                                        center = prop.centroid
+                                                        height =  abs(maxc - minc)
+                                                        width =  abs(maxr - minr)
+                                                        break
+                        
+                        Label = np.zeros([TotalCategories + 5])
+                        Label[trainlabel] = 1
+                        
+                        if x + shift[0]> sizeX/2 and y + shift[1] > sizeY/2 and x + shift[0] < image.shape[2] and y + shift[1] < image.shape[1]:
+                                    crop_Xminus = x + shift[0] - int(ImagesizeX/2)
+                                    crop_Xplus = x + shift[0] + int(ImagesizeX/2)
+                                    crop_Yminus = y + shift[1] - int(ImagesizeY/2)
+                                    crop_Yplus = y + shift[1] + int(ImagesizeY/2)
+              
+                                    region =(slice(int(time - 1),int(time)),slice(int(crop_Yminus), int(crop_Yplus)),
+                                           slice(int(crop_Xminus), int(crop_Xplus)))
+                                    crop_image = image[region]      
+                                    Label[TotalCategories] =  (center[1] - crop_Xminus)/sizeX
+                                    Label[TotalCategories + 1] = (center[0] -  crop_Yminus)/sizeY
+                                    Label[TotalCategories + 2] = height/ImagesizeY
+                                    Label[TotalCategories + 3] = width/ImagesizeX
                                    
+                                    #Object confidence is 0 for background label else it is 1
+                                    if trainlabel > 0:
+                                      Label[TotalCategories + 4] = 1
+                                    else:
+                                      Label[TotalCategories + 4] = 0 
+                                  
+                                    if(crop_image.shape[1]== ImagesizeY and crop_image.shape[2]== ImagesizeX):
+                                             imwrite((save_dir + '/' + name + '.tif'  ) , crop_image.astype('float32'))  
                                    
-                            if int(prop.centroid[0]) == int(TwoDCoordinates[closestpoint[1]][0]) and int(prop.centroid[1]) == int(TwoDCoordinates[closestpoint[1]][1]):
-                                                minr, minc, maxr, maxc = prop.bbox
-                                                center = prop.centroid
-                                                height =  abs(maxc - minc)
-                                                width =  abs(maxr - minr)
-                                                break
-                
-                Label = np.zeros([TotalCategories + 5])
-                Label[trainlabel] = 1
-                
-                if x + shift[0]> sizeX/2 and y + shift[1] > sizeY/2 and x + shift[0] < image.shape[2] and y + shift[1] < image.shape[1]:
-                            crop_Xminus = x + shift[0] - int(ImagesizeX/2)
-                            crop_Xplus = x + shift[0] + int(ImagesizeX/2)
-                            crop_Yminus = y + shift[1] - int(ImagesizeY/2)
-                            crop_Yplus = y + shift[1] + int(ImagesizeY/2)
-      
-                            region =(slice(int(time - 1),int(time)),slice(int(crop_Yminus), int(crop_Yplus)),
-                                   slice(int(crop_Xminus), int(crop_Xplus)))
-                            crop_image = image[region]      
-            
-                            Label[TotalCategories] =  center[1]/sizeX
-                            Label[TotalCategories + 1] = center[0]/sizeY
-                            Label[TotalCategories + 2] = height/ImagesizeY
-                            Label[TotalCategories + 3] = width/ImagesizeX
-                               
-                           
-                            #Object confidence is 0 for background label else it is 1
-                            if trainlabel > 0:
-                              Label[TotalCategories + 4] = 1
-                            else:
-                              Label[TotalCategories + 4] = 0 
-                          
-                            if(crop_image.shape[1]== ImagesizeY and crop_image.shape[2]== ImagesizeX):
-                                     imwrite((save_dir + '/' + name + '.tif'  ) , crop_image.astype('float32'))  
-                           
-                            writer = csv.writer(open(save_dir + '/' + (name) + ".csv", "w"))
-                            for l in Label : writer.writerow ([l])
+                                    writer = csv.writer(open(save_dir + '/' + (name) + ".csv", "w"))
+                                    for l in Label : writer.writerow ([l])
 
        
 
-      
+       except:
+          
+          pass
 
 
 
